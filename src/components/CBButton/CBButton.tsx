b4cea8ef3ee32b3efbd3af6eef6fd1bcefe788f4
Cristian Donnachie <119471458+c-donnachie@users.noreply.github.com>
2024-04-05 00:51:11 -0300

    styles and things were added to the homescreen

---
+++ src/components/CBButton/CBButton.tsx
@@ -0,0 +1,68 @@
+import React, { FC } from 'react'
+import { StyleSheet, Text, View, TouchableHighlight, Animated } from 'react-native'
+import * as HapTics from 'expo-haptics'
+import Colors from '@/constants/Colors'
+
+interface CCButonProps {
+    title: string
+}
+
+export const CBButton: FC<CCButonProps> = ({ title }) => {
+
+    const animatedValue = new Animated.Value(1)
+
+    const onPressIn = () => {
+        Animated.spring(animatedValue, {
+            toValue: 0.9,
+            useNativeDriver: true
+        }).start()
+    }
+
+    const onPressOut = () => {
+        Animated.spring(animatedValue, {
+            toValue: 1,
+            useNativeDriver: true
+        }).start()
+    }
+
+    const animatedStyle = {
+        transform: [{ scale: animatedValue }]
+    }
+
+    return (
+        <Animated.View style={[s.container, animatedStyle]}>
+            <TouchableHighlight
+                style={{borderRadius: 10}}
+                onPressIn={onPressIn}
+                onPressOut={onPressOut}
+                onPress={() => {
+                    HapTics.impactAsync(HapTics.ImpactFeedbackStyle.Light)
+                }}
+            >
+                <View style={s.button}>
+                    <Text style={s.text}>{title}</Text>
+                </View>
+            </TouchableHighlight>
+        </Animated.View>
+    )
+}
+
+const s = StyleSheet.create({
+    container: {
+        width: '85%',
+        borderRadius: 8
+    },
+    button: {
+        width: '100%',
+        height: 57,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: Colors.cbBlue,
+        borderRadius: 10,
+    },
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+})
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,10 +1,47 @@
+import { StyleSheet, Text, View, SafeAreaView, ScrollView, Image } from 'react-native'
 import React from 'react'
-import {View, Text} from 'react-native'
+import Colors from '@/constants/Colors'
+import { CBButton } from '@/components/CBButton/CBButton'
 
 export const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={s.container}>
+      <ScrollView
+        contentContainerStyle={{ alignItems: 'center' }}
+      >
+        <Image
+          style={s.image}
+          source={{ uri: 'https://imgur.com/9EEaSaS.png' }}
+        />
+        <Text style={s.title}>Welcome to Coinbase!</Text>
+        <Text style={s.subTitle}>Make your first investment today</Text>
+        <CBButton title='Get started' />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    backgroundColor: '#fff'
+  },
+  image: {
+    height: 250,
+    width: 150,
+    marginTop: 40
+  },
+  title: {
+    fontSize: 21,
+    fontWeight: '600',
+    marginBottom: 8,
+    letterSpacing: .5,
+  },
+  subTitle: {
+    fontSize: 17,
+    marginBottom: 24,
+    color: Colors.subTitle
+  }
+
+})
